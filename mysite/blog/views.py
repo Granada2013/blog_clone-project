1db8f87840b625aa0248652abf15b422944046cf
Anna <aspitsyna91@gmail.com>
2020-07-09 19:28:46 +0700

    Views.py and urls.py uopdated

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.utils import timezone
 from .forms import PostForm, CommentForm
@@ -15,10 +16,11 @@ class HomeView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date') #('-pub_date' = DESC)
-            # SELECT * FROM Post
-            # WHERE published_date <= now
-            # ORDER BY published_date DESC
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by(
+            '-published_date')  # ('-pub_date' = DESC)
+        # SELECT * FROM Post
+        # WHERE published_date <= now
+        # ORDER BY published_date DESC
 
 
 class AboutView(TemplateView):
@@ -29,16 +31,16 @@ class AboutView(TemplateView):
 
 
 class PostDetailView(DetailView):
-    '''
+    """
     Renders post details
-    '''
+    """
     model = Post
 
 
 class CreatePostView(LoginRequiredMixin, CreateView):
-    '''
+    """
     Renders a form to create a new post
-    '''
+    """
     login_url = '/login'
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
@@ -46,9 +48,9 @@ class CreatePostView(LoginRequiredMixin, CreateView):
 
 
 class UpdatePostView(LoginRequiredMixin, UpdateView):
-    '''
+    """
     Renders a form to update an existing post
-    '''
+    """
     login_url = '/login'
     redirect_field_name = 'blog/post_detail.html'
     form_class = PostForm
@@ -56,17 +58,17 @@ class UpdatePostView(LoginRequiredMixin, UpdateView):
 
 
 class DeletePostView(LoginRequiredMixin, DeleteView):
-    '''
+    """
     Renders a confirm_delete form
-    '''
+    """
     model = Post
     success_url = reverse_lazy('blog:home')
 
 
 class DraftListView(LoginRequiredMixin, ListView):
-    '''
+    """
     Renders a list of unpublished posts
-    '''
+    """
     login_url = '/login/'
     redirect_field_name = 'blog/post_detail.html'
     model = Post
@@ -75,6 +77,50 @@ class DraftListView(LoginRequiredMixin, ListView):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
 
 
+@login_required
+def publish_post(request, pk):
+    """
+    Realizes .publish() method of class Post
+    """
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog:home', pk=pk)
 
 
+def add_comment(request, pk):
+    """
+    Renders a comment form and saves a new comment to a database
+    """
+    post = get_object_or_404(Post, pk=pk)
 
+    if request.method == 'POST':
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('blog:post_detail', pk=pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def approve_comment(request, pk):
+    """
+    Realizes the .approve() metnod of class Comment
+    """
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('blog:post_detail', pk=comment.post.pk)
+
+
+@login_required
+def remove_comment(request, pk):
+    """
+    Realizes built-in .delete() method of class Comment
+    """
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('blog:post_detail', pk=post_pk)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.auth_login, name='login'),
+    path('accounts/logout/', views.auth_logout, name='logout', kwargs={'next_page': 'blog:home'}),
 ]
