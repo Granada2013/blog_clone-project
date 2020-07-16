ac049f4566d5dad0054eb5a6dead3b3177709376
Anna <aspitsyna91@gmail.com>
2020-07-16 13:32:15 +0700

    myenv updated

---
+++ myenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\anna\pycharmprojects\django_advanced\social_project\myenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
